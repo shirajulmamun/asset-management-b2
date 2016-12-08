dfd25b021172d940c2950d1dc6d3f0686c6805ed
shirajul.mamun <shirajul.mamun@gmail.com>
2016-12-08 23:15:19 +0600

    <add> EmployeeDesignation - Organization Model

--- AssetTracker/AssetTracker.Core/Models/Organization.cs
+++ AssetTracker/AssetTracker.Core/Models/Organization.cs
@@ -8,19 +8,11 @@ namespace AssetTracker.Core.Models
 {
     public class Organization
     {
-        //public Organization()
-        //{
-            
-        //}
-        //public Organization(string name, string code):this()
-        //{
-        //    this.Name = name;
-        //    this.Code = code;
-        //}
         public int Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public string Location { get; set; }
+        public string EmployeeDesignation { get; set; }
 
         public ICollection<OrganizationBranch> OrganizationBranches { get; set; } 
     }
